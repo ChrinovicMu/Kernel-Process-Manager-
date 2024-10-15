a360caa146b14a271c09b088624e61017b0b505d
ChrinovicMu <chrinovicmu@gmail.com>
2024-10-15 22:13:21 +0200

    thread array fixes

--- src/main.c
+++ src/main.c
@@ -5,7 +5,8 @@
 #include <pthread.h>
 #include <errno.h>
 #include <unistd.h>
-#define MEMORY_LIMIT 1024
+#include <string.h>
+#define MEMORY_LIMIT 150
 
 #define MEM_BLOCK_SIZE sizeof(struct Mem_Block)
 pthread_mutex_t process_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -53,26 +54,25 @@ struct Process{
     int kill: 1;            //ignore 
     struct Context context; // register usage
 };
-
-void push_P(struct Process * p, struct Kernel_Info * kernel_stack_info){
+int push_P(struct Process * p, struct Kernel_Info * kernel_stack_info){
 
     pthread_mutex_unlock(&process_mutex);
     struct Mem_Block *b = (struct Mem_Block *) malloc(sizeof(struct Mem_Block));
-    
+
     if (!b){
         fprintf(stderr,"allocation failed\n");
         pthread_mutex_unlock(&process_mutex);
-        return; 
+        return -1; 
     }
     if (p->state !=READY){
         fprintf(stderr, "process not ready for execution\n");
         pthread_mutex_unlock(&process_mutex);
-        return;
+        return -1;
     }
     if((kernel_stack_info->kernel_stack_used + MEM_BLOCK_SIZE + p->size) > (kernel_stack_info->kernel_stack_mem)){
         pthread_mutex_unlock(&process_mutex);
         fprintf(stderr, "Memory Full\n");
-        return;
+        return -1;
     }
     kernel_stack_info->kernel_stack_used += (p->size + MEM_BLOCK_SIZE);
     b->process = p;
@@ -80,8 +80,8 @@ void push_P(struct Process * p, struct Kernel_Info * kernel_stack_info){
     b->next_b = top;
     top = b;
     pthread_mutex_unlock(&process_mutex);
+    return 0;
 }
-
 void run_P(struct Process *p){
 
     pthread_mutex_unlock(&process_mutex);
@@ -112,13 +112,11 @@ void *thread_func(void *arg){
     run_P(p);
     return NULL;
 }
-
-void run_process_threads(struct Process *p1, struct Process *p2){
-    
+void run_process_threads(struct Process *p_array[], size_t len){
+    /*
     pthread_t thread1; 
     pthread_t thread2;
     int t_err;
-
     t_err = pthread_create(&thread1, NULL, thread_func, (void *)p1);
     if(t_err != 0){
         fprintf(stderr,"Error creating threads\n");
@@ -131,11 +129,28 @@ void run_process_threads(struct Process *p1, struct Process *p2){
     }
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
-
     pthread_mutex_destroy(&process_mutex);
+    */
+
+    pthread_t thread_arr[len];
+    int x, t_err; 
+    for(x = 0; x < len; ++x){
+        pthread_create(&thread_arr[x], NULL, thread_func, p_array[x]);
+        if(t_err != 0){
+            fprintf(stderr, "Error creating threads\n");
+            return; 
+        }
+    }
+    for(x = 0; x < len; ++x){
+        pthread_join(thread_arr[x], NULL);
+        if(t_err != 0){
+            fprintf(stderr, "Error joinning threads\n");
+            return;
+        }
+    }
 }
 
-struct Process * creatProcess(int id){
+struct Process * creatProcess(unsigned int id){
 
     struct Context context = {12, 13, 3,0,0,0,0};    //arbitury test values
     struct Process *p = (struct Process *)malloc(sizeof(struct Process));
@@ -144,7 +159,7 @@ struct Process * creatProcess(int id){
         return NULL;
     }
     p->size = sizeof(p);
-    p->state = UNUSED;
+    p->state = READY;
     p->pid = id;
     p->slp = 0;
     p->kill = 0;
@@ -176,25 +191,38 @@ int main(int argc, char *argv[])
     kernel_stack_info->kernel_stack_used = 0;
     struct Process *p1 = NULL;
     struct Process *p2 = NULL;
+    struct Process *p3 = NULL;
+    struct Process *p4 = NULL;
+
+    p3 = creatProcess(2222);
+    p4 = creatProcess(4444);
 
     p1 = creatProcess(3333);
     p2 = creatProcess(9999);
-    if(!p1 || !p2){
+    if(!p1 || !p2 || !p3 || !p4){
         fprintf(stderr, "memory allocation of process failed\n");
         free(p1);
         free(p2);
+        free(p3);
+        free(p4);
         return -1;
     }
-    p1->state = READY;
-    p2->state = READY;
 
-    push_P(p1, kernel_stack_info);
-    push_P(p2, kernel_stack_info);
+    int push1, push2, push3, push4;
 
-    struct Process *p_Array[] = {p1, p2};
-    size_t len = sizeof(p_Array) / sizeof(p_Array[0]);
+    push1 = push_P(p1, kernel_stack_info);
+    push2 = push_P(p2, kernel_stack_info);
+    push3 = push_P(p3, kernel_stack_info);
+    push4 = push_P(p4, kernel_stack_info);
 
-    run_process_threads(p1, p2);
+    if(push1 != 0 || push2 != 0 || push3 != 0 || push4 != 0){
+        fprintf(stderr, "Couldn't push to the memory\n");
+        return -1;
+    }
+    struct Process *p_array[4] = {p1, p2, p3, p4};
+    size_t len = sizeof(p_array)/ sizeof(p_array[0]);
+
+    run_process_threads(p_array, len);
 
     //JUST FOR ANALYSIS AND TESTING OF Kill_p()
     printf("BEFORE KILL :\n");
